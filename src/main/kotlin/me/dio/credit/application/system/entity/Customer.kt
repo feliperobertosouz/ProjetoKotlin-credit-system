41bc10df3199117cb408b610e4797575db98c4fd
Felipe Roberto <56833987+feliperobertosouz@users.noreply.github.com>
2023-10-18 10:41:48 -0300

    feat: Created the repositorys of entitys and migrations

--- src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -1,11 +1,16 @@
 package me.dio.credit.application.system.entity
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "Customer")
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    var cpf: String = "",
-    var email: String = "",
-    var address: Address = Address(),
-    var credits: List<Credit> = listOf(),
-    var id: Long? = null,
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) val cpf: String = "",
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST)
+    , mappedBy = "customer")  var credits: List<Credit> = listOf(),
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY) var id: Long? = null,
 )
